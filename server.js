b4fa929854b18baf5a333bdf7ad7b9a172bcd954
palhamel <pal.hamel@gmail.com>
2020-05-07 10:32:06 +0200

    Setup mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -12,6 +13,13 @@ const app = express()
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
+
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost/animals'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+
+
 
 // Start defining your routes here
 app.get('/', (req, res) => {
